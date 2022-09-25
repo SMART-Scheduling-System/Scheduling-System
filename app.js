cc3f4712fc69ce8bb1433f526a47bce7ac5128f9
KeravNahar <kerav.nahar19@st.niituniversity.in>
2022-09-25 19:06:04 +0530

    Adding graphql for api testing

--- app.js
+++ app.js
@@ -1,14 +1,41 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
+const graphqlHttp = require('express-graphql').graphqlHTTP;
+const { buildSchema } = require('graphql');
 
 const app = express();
 
 app.use(bodyParser.json());
 
+app.use('/graphql', graphqlHttp({
+    schema: buildSchema(`
+    type RootQuery {
+        events: [String!]!
+    }
+
+    type RootMutation {
+        createEvent(name: String): String
+    }
+
+    schema {
+            query: RootQuery
+            mutation: RootMutation
+    }
+    `) ,
+    rootValue: {
+        events: () => {
+            return ['Setting','Up','Graphql'];
+        },
+        createEvent: (args) => {
+            const eventName = args.name;
+            return eventName;
+        }
+    },
+    graphiql: true
+}));
 
 app.listen(3000);
 
-app.get('/', (req,res,next)=>{
-    res.send("Basic node setup initiated");
-})
+// app.get('/', (req,res,next)=>{
+//     res.send("Basic node setup initiated");
+// })
