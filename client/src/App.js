7dd38d2ce6abd46e1267c5f5e69a2db2f1c1e366
KeravNahar <kerav.nahar19@st.niituniversity.in>
2022-10-11 23:56:05 +0530

    Project Setup Remodeled

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import 'antd/dist/antd.min.css'
+import {Button} from 'antd'
+
+function App() {
+  return (
+    <div className="App p-5">
+      <h1>SMART-Scheduling System</h1>
+      <Button type="primary">Primary Button</Button>
+    </div>
+  );
+}
+
+export default App;
