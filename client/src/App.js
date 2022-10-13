69b498f79679505f89088b2c7b9d1fadd05e93f7
Prabh289 <prabhsethi289@gmail.com>
2022-10-13 23:08:56 +0530

    Login-Register Page

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,17 @@
 import React from "react";
-import 'antd/dist/antd.min.css'
-import {Button} from 'antd'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import { Button } from 'antd';
 
 function App() {
   return (
-    <div className="App p-5">
-      <h1>SMART-Scheduling System</h1>
-      <Button type="primary">Primary Button</Button>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path='/register' element={<Register />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
+import 'antd/dist/antd.min.css'
 import ReactDOM from 'react-dom/client';
 import './index.css';
+
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
