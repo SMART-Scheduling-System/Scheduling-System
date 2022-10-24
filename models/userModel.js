af91c8b5a5a1e3a99ff6336d36e5c41dac4a9e80
KeravNahar <kerav.nahar19@st.niituniversity.in>
2022-10-24 17:59:54 +0530

    Edited files for login after test

--- models/userModel.js
+++ models/userModel.js
@@ -5,7 +5,7 @@ const userSchema = new mongoose.Schema(
   {
     name: {
       type: String,
-      required: true,
+      required: false,
     },
     email: {
       type: String,
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -11,7 +11,7 @@ router.post("/register", async (req, res) => {
     const userExists = await User.findOne({email: req.body.email});
     if(userExists)
     {
-      return res.status(400).send({ message : "User already exists", success:false});
+      return res.status(400).send({message:[{ message : "User already exists", success:false}]});
     }
     const password = req.body.password;
     const salt = await bcrypt.genSalt(10);
@@ -20,10 +20,14 @@ router.post("/register", async (req, res) => {
 
     req.body.password = hashedPassword;
 
-    const newuser = new User(req.body);
+    const newuser = new User({
+      name: req.body.name,
+      email:req.body.email,
+      password: hashedPassword
+    });
 
     await newuser.save();
-    req.statusCode(200).send({ message:"User created successfully", success:true});
+    req.statusCode(200).send({ message:[{ message: "User created successfully", success:true}]});
 
   } catch (error) {
     console.log(error);
@@ -38,19 +42,19 @@ router.post("/login", async (req,res) =>{
     if (!user){
       return res
         .status(200)
-        .send( { message:"User does not exist", success:false });
+        .send( {message:[{ message : "User does not exists", success:false}]});
     }
     const isMatch = await bcrypt.compare(req.body.password, user.password);
     if(!isMatch){
-      return res.status(200).send( {message:"Password is incorrect", success:false});
+      return res.status(200).send( {message:[{ message : "Password is incorrect", success:false}]});
     }
     else{
       const token = jwt.sign( { id: user._id }, process.env.JWT_SECRET, { expiresIn:"1d"});
-      res.status(200).send( { message:"Login successful", success: true, data:token});
+      res.status(200).send( {message:[{ message:"Login successful", success: true, data:token}]});
     }
   }catch(error) {
     console.log(error);
-    res.status(500).send( {message:"Error logging in", success:false, error});
+    res.status(500).send( {message:[{message:"Error logging in", success:false, error}]});
   }
 }
 
