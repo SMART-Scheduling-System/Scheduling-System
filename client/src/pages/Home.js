f9a5c0b7af21cc148502212879816dc6f6fa24c1
Aanya Mittal <aanyanitinmittal@gmail.com>
2022-10-29 01:45:24 +0530

    step 5 done

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,16 +1,27 @@
-import React, { useEffect } from "react";import axios from "axios";
-
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import Layout from "../components/Layout";
+import { Col, Row } from "antd";
+import Doctor from "../components/Doctor";
+import { useDispatch, useSelector } from "react-redux";
+import { showLoading, hideLoading } from "../redux/alertsSlice";
 function Home() {
+  const [doctors, setDoctors] = useState([]);
+  const dispatch = useDispatch();
   const getData = async () => {
     try {
-      const response = await axios.get("/api/user/get-user-info-by-id", {}, {
+      dispatch(showLoading())
+      const response = await axios.get("/api/user/get-all-approved-doctors", {
         headers: {
           Authorization: "Bearer " + localStorage.getItem("token"),
         },
       });
-      console.log(response.data);
+      dispatch(hideLoading())
+      if (response.data.success) {
+        setDoctors(response.data.data);
+      }
     } catch (error) {
-      console.log(error);
+      dispatch(hideLoading())
     }
   };
 
@@ -18,8 +29,16 @@ function Home() {
     getData();
   }, []);
   return (
-    <div>Login Home</div>
-  )
+    <Layout>
+      <Row gutter={20}>
+        {doctors.map((doctor) => (
+          <Col span={8} xs={24} sm={24} lg={8}>
+            <Doctor doctor={doctor} />
+          </Col>
+        ))}
+      </Row>
+    </Layout>
+  );
 }
 
-export default Home
+export default Home;
